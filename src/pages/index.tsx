4230963e98329f72c1b078ea12e41be9a51bd72d
IranildoHGJr <iranildojr.11@gmail.com>
2022-03-23 09:55:27 -0300

    update 0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,19 @@ import Head from 'next/head'
 
 import rocketseatLogo from '../assets/rocketseatLogo.svg'
 
+import { Container } from '../styles/pages/home'
+
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Homepage</title>
       </Head>
 
-      <main>
-        <img src={rocketseatLogo} alt="rocketseatLogo" />
-        <h1>Hello World!</h1>
-      </main>
-
-    </div>
+      <img src={rocketseatLogo} alt="rocketseatLogo" />
+      <h1>ReactJs Structure</h1>
+      <p>A ReactJS + Next.js structure made by Rocketseat.</p>
+    </Container>
   )
 }
 
